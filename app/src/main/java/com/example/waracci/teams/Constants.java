4666ee450742cea263231e240e2ba6030aaca3ec
waracci <morriswarachi0@gmail.com>
2017-09-23 10:07:15 +0300

    constnts .java file contains constant 'variables' that are least likely to change throughout the app's lifecycle and are included here for use and reuse with much ease, once altered here in this file, changes are reflected everywhere

---
+++ app/src/main/java/com/example/waracci/teams/Constants.java
@@ -0,0 +1,13 @@
+package com.example.waracci.teams;
+
+/**
+ * Created by waracci on 9/19/17.
+ */
+
+public class Constants {
+    public static final String BASE_URL = "http://www.thesportsdb.com/api/v1/json/1/searchplayers.php?";
+    public static final String QUERY_PARAMETER = "t";
+    public static final String PREFERENCES_TEAM_KEY = "team";
+    public static final String FIREBASE_CHILD_SEARCHED_TEAMS = "searchedTeam";
+    public static final String FIREBASE_CHILD_SPORT_TEAMS = "teams";
+}
