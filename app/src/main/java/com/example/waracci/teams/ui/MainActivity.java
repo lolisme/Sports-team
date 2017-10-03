b1cb8c3c3334470a7e4946ccd5a09ae9e9015ced
waracci <morriswarachi0@gmail.com>
2017-10-03 07:43:46 +0300

    it will include an overflow menu

--- app/src/main/java/com/example/waracci/teams/ui/MainActivity.java
+++ app/src/main/java/com/example/waracci/teams/ui/MainActivity.java
@@ -2,34 +2,25 @@ package com.example.waracci.teams.ui;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.waracci.teams.Constants;
-import com.example.waracci.teams.ui.PlayersActivity;
 import com.example.waracci.teams.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-import static com.example.waracci.teams.R.id.team;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private SharedPreferences mSharedPreferences;
