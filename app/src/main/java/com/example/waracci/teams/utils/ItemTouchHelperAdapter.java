fe2d3c286cbd60fdea3340307a42daf06198cc6f
waracci <morriswarachi0@gmail.com>
2017-10-03 07:45:12 +0300

    it will instantiate the touch view helper interface for implementing touch gestures

--- app/src/main/java/com/example/waracci/teams/utils/ItemTouchHelperAdapter.java
+++ app/src/main/java/com/example/waracci/teams/utils/ItemTouchHelperAdapter.java
@@ -4,5 +4,7 @@ package com.example.waracci.teams.utils;
  * Created by waracci on 10/2/17.
  */
 
-public class ItemTouchHelperAdapter {
+public interface ItemTouchHelperAdapter {
+    boolean onItemMove(int fromPosition, int toPosition);
+    void onItemDismiss(int position);
 }
